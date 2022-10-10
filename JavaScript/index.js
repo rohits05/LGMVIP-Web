75a215d9be05151d5e8bdc453aa75c6d516586c8
Sourav Chandra <86641474+souravchandra1910@users.noreply.github.com>
2022-10-10 09:38:42 +0530

    index.js
    Added javascript

---
+++ JavaScript/index.js
@@ -0,0 +1,15 @@
+let c;
+c=true;
+console.log(c);
+let d=null;
+console.log(d);
+
+
+let num=10;
+for(let i=0;i<num;i++){
+    console.log(i+1,''); 
+}
+let str = strArray.join("##");
+console.log(str);
+let s = doubleQuote.trim();
+console.log(s);
